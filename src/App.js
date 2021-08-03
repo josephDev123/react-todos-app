ce9ec8e2febf34363f1a9b4a014cc1ab5f483b43
uzuegbu joseph <josephuzuegbu55@gmail.com>
2021-08-03 20:25:24 +0100

    first deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='row justify-content-center text-center'>
+        <div className='col-md-6'>
+          <h3>React Todos App</h3>
+          <form>
+              <div className="mb-3">
+                <label htmlfor="exampleInputEmail1" class="form-label">Todos Content</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+              </div>
+          </form>
+
+          <ul className="list-group">
+            <li className="list-group-item d-flex justify-content-between align-items-center">
+              A list item
+              <span className="badge bg-danger rounded-pill"><i className="fa fa-trash"></i></span>
+            </li>
+            
+          </ul>
+
+        </div>
+
+      </div>
+    
     </div>
   );
 }
