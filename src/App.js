f69c764c6e2aa4a826c78bc1e469667b592f380e
uzuegbu joseph <josephuzuegbu55@gmail.com>
2021-08-10 20:18:08 +0100

    redux added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,63 @@
 
 import './App.css';
 import {addTodo} from './Component/reducers';
+import { useDispatch } from 'react-redux';
 import {useSelector} from 'react-redux';
+import { useState } from 'react';
+
 
 function App() {
+  const action = useDispatch();
+ 
+  // state
+const [EditForm, setEditForm] =useState(false);
+const [text, setText] =useState('');
 
-  const handleSubmit =()=>{
+const onHandleText =(e)=>{
+  setText(e.target.value)
+}
 
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    const content_el =document.getElementById('content');
+    if(text ==''){
+      console.log('Content field is empty');
+    }else{
+      action(addTodo({
+        id:Date.now(),
+        text:text
+      }))
+      content_el.value ='';
+    } 
   }
+
+  const list =useSelector(state => state.todo);
+  console.log(list);
   
   return (
     <div className="container">
       <div className='row justify-content-center text-center'>
         <div className='col-md-8'>
           <h3>React Todos App</h3>
-          <form>
-              <div className="mb-3">
+          <form onSubmit ={handleSubmit}>
+              <div className="mb-3" >
                 <label htmlFor="content" className="form-label">Todos Content</label>
-                <input type="text" className="form-control" id="content" aria-describedby="content" onClick ={handleSubmit}/>
+                <input type="text" className="form-control" id="content" aria-describedby="content" onInput ={onHandleText}/>
               </div>
           </form>
 
           <ul className="list-group">
             <li className="list-group-item d-flex justify-content-between align-items-center">
               A list item
-              <span className="badge bg-primary rounded-pill" style={{cursor:'pointer'}}>Edit</span>
+
+              {
+                // determine what to show btw edit text and edit btn
+              (EditForm ==false)?
+              <span className="badge bg-primary rounded-pill" style={{cursor:'pointer'}} onClick={()=>setEditForm(true)}>Edit</span>:
               <span className="badge rounded-pill">
-                  <input type="text" className="form-control form-control-sm" placeholder="Edit todo" aria-label="edit todo" aria-describedby="edit todo"/>
-              </span>
+              <input type="text" className="form-control form-control-sm" placeholder="Edit todo" aria-label="edit todo" aria-describedby="edit todo"/>
+          </span>
+              }
               <span className="badge bg-danger rounded-pill" style={{cursor:'pointer'}}><i className="fa fa-trash"></i></span>
             </li>
             
---
+++ src/Component/List.js
@@ -0,0 +1,9 @@
+// import React from 'react'
+// import {useSelector} from 'react-redux';
+
+// let finalList='';
+// const lists =useSelector(state => state);
+// let finalisit = lists.map(list=> list.text);
+// console.log(finalisit);
+// export default finalList;
+
